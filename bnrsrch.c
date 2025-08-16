74e0d2ad665e0802f127a8cef98e5942b74c4b55
Kashi-techy <nathkashi591@gmail.com>
2025-08-16 22:38:34 +0530

    Add files via upload

---
+++ bnrsrch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Recursive binary search function
+int binarySearch(int arr[],int low,int high,int key) {
+    if (low > high)
+        return -1; // Key not found
+
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+    else if (key < arr[mid])
+        return binarySearch(arr, low, mid - 1, key); // Search left half
+    else
+        return binarySearch(arr, mid + 1, high, key); // Search right half
+}
+
+int main() {
+    int arr[100], n, key, result;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements in SORTED order : ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter the key to be searched : ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n-1, key);
+
+    if (result == -1)
+        printf("Key NOT found.\n");
+    else
+        printf("Key found at Index [%d].\n", result);
+
+    return 0;
+}
+
