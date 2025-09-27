ba6f50cb66db1c15763cce00c97b56b4b96fd967
Kashi-techy <nathkashi591@gmail.com>
2025-09-27 11:02:01 +0530

    Update exp12_queueSLL.c

--- exp12_queueSLL.c
+++ exp12_queueSLL.c
@@ -1,83 +1,114 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 #include <stdlib.h>
-struct Node
-{
+
+// Define the Node structure for the queue
+struct Node {
     int data;
     struct Node *next;
-}*front=NULL,*rear=NULL;
-void enqueue()
-{
-    struct Node *newnode=(struct Node*)malloc(sizeof(struct Node));
-    printf("Enter data:");
-    scanf("%d",&newnode->data);
-    newnode->next=NULL;
-    if(front==NULL)
-    {
-    front=newnode;
-    rear=newnode;
-    }
-    else
-    {
-    rear->next=newnode;
-    rear=newnode;
-    }
-    }
-int dequeue()
-{
-    if (front==NULL)
-    {printf("Queue is empty...underflow");return -1;}
-    else
-    {
-    struct Node *temp;
-    temp=front;
-    int rv=front->data;
-    front=front->next;
-    if(front==NULL) rear=NULL;
-    free(temp);
-    return(rv);
-    }
-}
-void displayQ()
-{
-    struct Node *temp=front;
-    while(temp!=NULL)
-    {
-    printf(" %d---->",temp->data);
-    temp=temp->next;
-    }
-    printf(" NULL");
-}
-void main()
-{
-    int ch;
+};
 
-do
-{
-printf("\nMenu\n----\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit");
-printf("\nEnter the choice:");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:
-    enqueue();
-    displayQ();
-    break;
-case 2:
-    int data;
-    data=dequeue();
-    if(data!=-1)
-        printf("The dequed data is:%d\n",data);
-    displayQ();
-    break;
-case 3:
-    displayQ();
-    break;
-case 4:
-    exit(0);
-default:
-    printf("There is no such operation:");
+// Global pointers for the front and rear of the queue
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+// Function to add an element to the rear of the queue (Enqueue)
+void enqueue(int value) {
+    // Create a new node
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Heap overflow\n");
+        return;
     }
-  }
-while(1);
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // If the queue is empty, the new node is both front and rear
+    if (front == NULL) {
+        front = newNode;
+        rear = newNode;
+    } else {
+        // Add the new node at the end of the queue and change rear
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+// Function to remove an element from the front of the queue (Dequeue)
+int dequeue() {
+    // If the queue is empty, return an error value
+    if (front == NULL) {
+        return -1; // Indicates queue underflow
+    }
+
+    // Store the front node and its data
+    struct Node *temp = front;
+    int dequeuedValue = front->data;
+
+    // Move front one node ahead
+    front = front->next;
+
+    // If front becomes NULL, then change rear to also be NULL
+    if (front == NULL) {
+        rear = NULL;
+    }
+
+    free(temp); // Free the old front node
+    return dequeuedValue;
+}
+
+// Function to display all elements in the queue
+void displayQ() {
+    if (front == NULL) {
+        printf("Queue is empty.\n");
+        return;
+    }
+    struct Node *temp = front;
+    printf("Queue: FRONT -> ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Main function with menu
+int main() {
+    int ch, data, dequeuedValue;
+
+    while (1) {
+        printf("\n--- Queue Menu ---\n");
+        printf("1. Enqueue (Add)\n");
+        printf("2. Dequeue (Remove)\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter the data to enqueue: ");
+                scanf("%d", &data);
+                enqueue(data);
+                displayQ();
+                break;
+            case 2:
+                dequeuedValue = dequeue();
+                if (dequeuedValue == -1) {
+                    printf("Queue underflow: Cannot dequeue from an empty queue.\n");
+                } else {
+                    printf("Dequeued value is: %d\n", dequeuedValue);
+                }
+                displayQ();
+                break;
+            case 3:
+                displayQ();
+                break;
+            case 4:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
 }
